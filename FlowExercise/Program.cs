41c587829036054a98c7c9f25d3edd7c3a16b48f
Mikael <test.kurs@hotmail.net>
2019-10-25 16:35:05 -0700

    Add project files.

---
+++ FlowExercise/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FlowExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool condition = true;
+            do
+            {
+                Console.WriteLine("Welcome to the Main menu");
+                Console.WriteLine("Please use numbers to navigate");
+                Console.WriteLine("Quit with 0");
+                int answer = int.Parse(Console.ReadLine());
+
+                switch (answer)
+                {
+                    case 0:
+                        condition = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Wrong Input!");
+                        break;
+                }
+            } while (condition);
+        }
+    }
+
+}
